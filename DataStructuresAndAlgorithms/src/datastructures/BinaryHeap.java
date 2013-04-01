863909d03a7507f34bce986d0cd8a33d17202236
Chandan Ravishankar <chandanpr@gmail.com>
2013-04-01 18:23:07 +0530

    Revert "Revert "Adding Data Structures and Algorithms content""
    This reverts commit 7df56dadd8c81fa722ce07245cba2a9bd42180f1.

---
+++ DataStructuresAndAlgorithms/src/datastructures/BinaryHeap.java
@@ -0,0 +1,79 @@
+package datastructures;
+
+import static utils.Utilities.*;
+
+/**
+ * For any given K:
+ * it's parent will be at k/2
+ * and children will be at 2k and 2k+1
+ * @author chandanpr
+ *
+ * @param <Key>
+ */
+public class BinaryHeap<Key extends Comparable<Key>> {
+
+	private Key[] data;
+	private int index;
+	
+	public BinaryHeap(int size){
+		this.data = (Key[])new Comparable[size];
+	}
+	
+	private void swim(int k){
+		//Find if parent is less than child
+		while(k > 1 && less(data[k/2], data[k])){
+			exchange(data, k, k/2);
+			k = k/2;
+		}
+	}
+	
+	private void sink(int k){
+		while(2*k <= index){
+			int j = 2*k;
+			//Find which child is bigger 2k or 2k+1
+			if(j < index && less(data[j], data[j+1])){
+				j++;
+			}
+			
+			//If item at k is less than item at j sink by exchange
+			if(!less(data[k], data[j])){
+				break;
+			}
+			
+			exchange(data, k, j);
+			k = j;
+			
+		}
+	}
+	
+	public boolean isEmpty(){
+		return index == 0;
+	}
+	
+	public void insert(Key key){
+		data[++index] = key;
+		swim(index);
+		showAll();
+	}
+	
+	/**
+	 * Atmost 2logN compares
+	 * @return
+	 */
+	public Key deleteMax(){
+		Key max = data[1];
+		exchange(data, 1, index--);
+		sink(1);
+		data[index+1] = null;
+		showAll();
+		return max;
+	}
+	
+	public void showAll(){
+		for(int i=0; i<data.length; i++){
+			System.out.print(data[i]+" , ");
+		}
+		System.out.println();
+	}
+	
+}
---
+++ DataStructuresAndAlgorithms/src/datastructures/UnorderedPriorityQueue.java
@@ -0,0 +1,60 @@
+package datastructures;
+
+import static utils.Utilities.*;
+public class UnorderedPriorityQueue<Key extends Comparable<Key>>{
+
+	private final Key[] data;
+	private int index;
+	
+	public UnorderedPriorityQueue(int size){
+		this.data = (Key[])new Comparable[size];
+	}
+
+	public void insert(Key key) {
+		data[index++] = key;
+	}
+	
+	public boolean isEmpty(){
+		return index == 0;
+	}
+	
+	public Key deleteMin(){
+		int min = 0;
+		for(int i=1; i<index; i++){
+			if(less(data[i], data[min])){
+				min = i;
+			}
+		}
+		exchange(data, min, index-1);
+		Key key = data[--index];
+		data[index] = null;
+		return key;
+	}
+	
+	public Key deleteMax(){
+		int max = 0;
+		for(int i=1; i<index; i++){
+			if(less(data[max], data[i])){
+				max = i;
+			}
+		}
+		exchange(data, max, index-1);
+		Key key = data[--index];
+		data[index] = null;
+		return key;
+	}
+	
+	public void show(){
+		for(int i=0; i<index; i++){
+			System.out.print(data[i]+" , ");
+		}
+		System.out.println();
+	}
+	
+	public void showAll(){
+		for(int i=0; i<data.length; i++){
+			System.out.print(data[i]+" , ");
+		}
+		System.out.println();
+	}
+}
