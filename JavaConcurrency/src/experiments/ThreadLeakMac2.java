9fde55143191b3113a267ded0a10b15559b0a9e6
Chandan Ravishankar <chandanpr@gmail.com>
2013-04-17 08:30:34 +0530

    Measure Construction Time also
    Measure Construction Time also

--- JavaConcurrency/src/experiments/ThreadLeakMac2.java
+++ JavaConcurrency/src/experiments/ThreadLeakMac2.java
@@ -10,13 +10,22 @@ public class ThreadLeakMac2 {
 	public static void main(String[] args) throws InterruptedException {
 		long threads = 0;
 		long max = 0;
+		long constructionMax = 0;
 		while (true) {
 			long time = System.currentTimeMillis();
 			Thread thread = new Thread();
+			time = System.currentTimeMillis() - time;
+			threads++;
+			
+			if (time > constructionMax || time > 1000) {
+				constructionMax = time;
+				System.out.println("Construction Time = " + time + ", threads = " + threads);
+			}
+			
+			time = System.currentTimeMillis();
 			thread.start(); // should finish almost immediately
 			time = System.currentTimeMillis() - time;
 			thread.join(); // short delay, hopefully
-			threads++;
 			if (time > max) {
 				max = time;
 				System.out.println("time = " + time + ", threads = " + threads);
