3bc235318e79dc08dcc5a5edbba76113da4968ec
Chandan Ravishankar <chandanpr@gmail.com>
2013-06-23 20:38:28 +0530

    STRING EXPAND IMPLEMENTATION
    STRING EXPAND IMPLEMENTATION

--- DataStructuresAndAlgorithms/src/experiments/StringQuestions.java
+++ DataStructuresAndAlgorithms/src/experiments/StringQuestions.java
@@ -15,16 +15,45 @@ public class StringQuestions {
 	}
 
 	private static String expand(char[] c) {
-		// TODO:
-		// CASES: number of times can be more than 1.
-		// First implement for 1 and then enhance
 		int N = c.length;
-		for (int i = N - 1; i >= 0; i++) {
+		int start = 0;
+		for (int i = 1; i < N; i++) {
 			if (Character.isDigit(c[i])) {
-				char c1 = c[i - 1];
+				int count = Character.digit(c[i], 10);
+				fill(c, start, i, count, c[i - 1]);
+				start += count;
 			}
 		}
-		return "";
+		return new String(c);
+	}
+
+	private static void fill(char[] c, int start, int current, int count,
+			char c1) {
+		if (count == 1)
+			return;
+		System.out.println("Fill 1"+new String(c)+" : "+ start+" : "+current+" : "+count+" : "+c1);
+		if (start + count > c.length)
+			throw new RuntimeException();
+		int shifts = count - current - start + 1;
+		if (shifts > 0) {
+			shift(current, c, shifts);
+		}
+		System.out.println("Fill 2"+new String(c));
+		for (int i = 0; i < count; i++) {
+			c[start + i] = c1;
+		}
+		System.out.println("Fill 3"+new String(c));
+	}
+
+	private static void shift(int current, char[] c, int shifts) {
+		int N = c.length;
+		for (int i = 0; i < shifts; i++) {
+			int last = N - 1;
+			while (last > current) {
+				c[last] = c[last - 1];
+				last--;
+			}
+		}
 	}
 
 	private static int countThePalindromes(String str) {
@@ -68,10 +97,13 @@ public class StringQuestions {
 		int length = str.length();
 		if (length == 1)
 			return true;
+		if (length == 2) {
+			return str.charAt(0) == str.charAt(1);
+		}
 		int middle = length / 2;
 		String first = str.substring(0, middle);
 		String second = str.substring(length % 2 == 0 ? middle : middle + 1);
-		return first.equals(new StringBuffer(second).reverse().toString());
+		return first.equals(reverse(second));
 	}
 
 	private static String replace(String input, String... replacers) {
@@ -102,33 +134,60 @@ public class StringQuestions {
 		}
 	}
 
-	private static char findFirstNonRepeatingCharacter(String str) {
+	private static char findLastNonRepeatingCharacter(String str) {
 		char[] chars = str.toCharArray();
 		BitSet set = new BitSet();
 		char uniqueChar = '\0';
 		for (char c : chars) {
-			if(!set.get(c)){
+			if (!set.get(c)) {
 				uniqueChar = c;
 				set.set(c);
-			}else if(c == uniqueChar){
+			} else if (c == uniqueChar) {
 				uniqueChar = '\0';
 			}
 		}
 		return uniqueChar;
 	}
 
+	private static String reverse(String str) {
+		char[] c = str.toCharArray();
+		int N = c.length;
+		for (int i = 0; i < N / 2; i++) {
+			char temp = c[N - i - 1];
+			c[N - i - 1] = c[i];
+			c[i] = temp;
+		}
+		return new String(c);
+	}
+
 	public static void main(String[] args) {
-		testRepeats();
-		testPalindromes();
-		testProgressiveSubString();
-		testFirstNonRepeatingCharacter();
+		// testRepeats();
+		// testPalindromes();
+		// testProgressiveSubString();
+		// testFirstNonRepeatingCharacter();
+		// testReverse();
+
+		testExpand();
+	}
+
+	private static void testExpand() {
+		assert expand("A1C2B4 ".toCharArray()).equals("ACCBBBB");
+		assert expand("A1C4B2 ".toCharArray()).equals("ACCCCBB");
+		//DOESN'T WORK FOR CASE WHERE FIRST ONE IS HAVING MORE COUNT???!!!!!
+//		assert expand("A4C1B2 ".toCharArray()).equals("AAAACBB");
+	}
+
+	private static void testReverse() {
+		assert reverse("ABC").equals("CBA");
+		assert reverse("ABCD").equals("DCBA");
 	}
 
 	private static void testFirstNonRepeatingCharacter() {
-		assert findFirstNonRepeatingCharacter("ababab") == '\0';
-		assert findFirstNonRepeatingCharacter("ababcab") == 'c';
-		assert findFirstNonRepeatingCharacter("ababcabdc") == 'd';
-		assert findFirstNonRepeatingCharacter("abadbasdsakdkjsadbab") == 'j';
+		assert findLastNonRepeatingCharacter("ababab") == '\0';
+		assert findLastNonRepeatingCharacter("abdababf") == 'f';
+		assert findLastNonRepeatingCharacter("ababcab") == 'c';
+		assert findLastNonRepeatingCharacter("ababcabdc") == 'd';
+		assert findLastNonRepeatingCharacter("abadbasdsakdkjsadbab") == 'j';
 	}
 
 	private static void testProgressiveSubString() {
---
+++ JavaLanguage/src/basics/Bits.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Bits {
+	
+	public static void main(String[] args) {
+		System.out.println(32 >> 1);//divide by 2
+		System.out.println(32 >> 3);//divide by 2pow3
+		System.out.println(32<<1);//multiply by 2
+		System.out.println(32<<3);//multiply by 2pow3
+		System.out.println(32>>>1);//unsigned right shift
+	}
+
+}
